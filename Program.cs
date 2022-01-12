4e148c3c43730f8013c28ff7d5e8ddd3d40d0413
drosenzweig2024 <89942068+drosenzweig2024@users.noreply.github.com>
2022-01-12 13:07:05 -0800

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace drosenzweig2024_BinaryNumbersInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
